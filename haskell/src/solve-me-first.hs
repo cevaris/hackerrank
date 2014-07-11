24dae25938ef40e671f89ba2c7ed34b417dace06
Adam Cardenas <cevaris@gmail.com>
2014-07-11 13:16:17 -0600

    first commit

---
+++ haskell/src/solve-me-first.hs
@@ -0,0 +1,31 @@
+#!/usr/bin/env runhaskell
+
+--This is an introductory challenge. The purpose of this challenge is to give you a working I/O template in your preferred language. It includes scanning 2 integers from STDIN, calling a function, returning a value, and printing it to STDOUT.
+
+--Your task is to scan two numbers from STDIN, and print the sum A+B on STDOUT.
+
+--Note: The code has been saved in a template, which you can submit if you want.
+
+--Input Format
+--Given A and B on two different lines.
+
+--Output Format
+--An integer that denotes Sum (A + B)
+
+--Constraints
+--1 ≤ A, B ≤ 1000
+
+--Sample Input
+
+--2
+--3
+
+--Sample Output
+
+--5
+
+main = do
+    left  <- getLine
+    right <- getLine
+    let sum = (read left  :: Int) + (read right  :: Int)
+    print sum
