b3fd1426d4a616574473e0ad135c0aabf2978269
Cevaris <cevaris@gmail.com>
2014-07-20 15:11:33 -0600

    can now tie utopian input into tree alto

--- haskell/src/utopian-tree.hs
+++ haskell/src/utopian-tree.hs
@@ -49,17 +49,12 @@ import Control.Monad
 
 --N = 4:
 --the height of the tree at the end of the 4th cycle = 7
+
+
 tree :: Char -> Integer -> Integer -> Integer
 tree _ height 0 = height
 tree 'M' height cycle = tree 'S' (height+1) (cycle-1)
 tree 'S' height cycle = tree 'M' (height*2) (cycle-1)
-
---utopia :: Integer -> [Integer] -> [Integer] -> IO ()
---utopia 0 x = x
---utopia testCount x = do
---                    line <- getLine
---                    print (tree 'S' 1 (read line :: Integer))
---                    utopia (testCount-1) x
 
 get2Lines = do
     line1 <- getLine
@@ -67,55 +62,19 @@ get2Lines = do
     return ((read line1 :: Integer),(read line2:: Integer))
 
 
---readInt :: () -> Integer
+readInt :: IO Integer
 readInt = do
         line <- getLine
         return (read line :: Integer)
 
---readTests :: Integer -> [String] -> [String]
---readTests 0 xs = xs
---readTests testCount xs = do 
---                    readTests (testCount-1) ((lift getLine):xs)
-
-
-
---readLines :: IO [String]
---readLines = do  c <- getLine
---                if c == "" 
---                    then return []
---                    else do s <- readLines
---                            return (c:s)
-
---readInts :: IO [Integer]
---readInts = do   c <- getLine
---                if c == "" 
---                    then return []
---                    else do s <- readInts
---                            return ((read c :: Integer):s)
-
---readInts :: Integer -> Integer
---readInts 0 = []
---readInts x = do
---            c <- getLine
---            s <- readInts (x-1)
---            return ((read c :: Integer):s)
 
 
 main = do
     testCount <- getLine
-    inputs <- replicateM (read testCount) readInt
-    print inputs
-    --hieghts <- readInts
-    --print hieghts
-    --lines <- readLines
-    --let hieghts = readTests (read testCount :: Integer) []
-    --return hieghts
-    --print 32
-    --print x
-    --let testCount' = (read testCount :: Integer)
-    --    tests = readTests testCount'  []
-    --print 10
-    --utopia (read testCount :: Integer) []
+    cycles <- replicateM (read testCount) readInt
+    let x = [ (tree 'S' 1 c) | c <- cycles] 
+    print x
+
 
 
 
