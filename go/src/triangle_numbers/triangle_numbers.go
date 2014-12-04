82a1c1589f6644dd82369edc5c6938d866d949f6
Adam Cardenas <cevaris@gmail.com>
2014-12-03 20:23:01 -0700

    ready to submit

--- go/src/triangle_numbers/triangle_numbers.go
+++ go/src/triangle_numbers/triangle_numbers.go
@@ -1,62 +1,14 @@
 package main
 
-/*
-Problem Statement
-
-Given a triangle of numbers where each number is equal to the sum of the three numbers on top of it, find the first even number in a row.
-
-Explanatory Note: The vertex of the triangle (at the top) is 1. The structure of the triangle is shown below. Each number is equal to the sum of the numbers at the following positions: Position X: immediately above it. Position Y: to the immediate left of X. Position Z: to the immediate right of X. If there are no numbers at positions X, Y, or Z, then assume those positions are occupied by a zero (0). This can occur for positions at the edge.
-
-Here are four rows of the triangle:
-
-          1
-      1   1  1
-   1  2   3  2  1
-1  3  6   7  6  3  1
-
-Input Format and Constraints
-First line contains a number T (number of test cases).
-Each of the next T lines contain a number N (the row number, assuming that the top vertex of the triangle is Row 1).
-
-Output Format
-For each test case, display an integer that denotes the position of the first even number.
-Note: Assume that the left most number in a row is Position 1.
-If there is no even number in a row, print -1.
-
-Constraints
-1<=T<=100
-3<=N<=1000000000
-
-Sample Input
-2
-3
-4
-
-Sample Output
-2
-3
-*/
-
-
 import (
 	"fmt"
 	"os"
 	"log"
-	// "strings"
 	"bufio"
-	// "io/ioutil"
 	"strconv"
-	//"encoding/binary"
 	"errors"
 )
 
-/*
->>> def pascal(n):
-...   line = [1]
-...   for k in range(n):
-...     line.append(line[k] * (n-k) / (k+1))
-...   return line
-*/
 func genPascalRow(n int) ([]int, error) {
 	pascalRow := make([]int, n+1)
 	pascalRow[0] = 1
@@ -77,27 +29,29 @@ func firstEven(arr []int) (int, error) {
 	return -1, errors.New("Even value not found")
 }
 
-func init(){
-	// Enable line numbers in output
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-}
 
 func main() {
 	testCases, _ := parseHackerRank()
-	//fmt.Println("test Count", testCases, err)
 
 	for _, t  := range testCases {
 		if pascalRow, err := genPascalRow(t+1); err != nil {
 			log.Println(err)
 		} else {
+			// Need to handle the offbyone answer
 			if ret, err := firstEven(pascalRow); err != nil {
-				fmt.Println(pascalRow, ret)
+				fmt.Println(ret)
 			} else {
-				fmt.Println(pascalRow, ret+1)
+				fmt.Println(ret+1)
 			}
 		}
 	}
 	
+}
+
+
+func init(){
+	// Enable line numbers in output
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 
@@ -134,3 +88,4 @@ func parseHackerRank() ([]int, error) {
 	}
 	return testCases, nil
 }
+
