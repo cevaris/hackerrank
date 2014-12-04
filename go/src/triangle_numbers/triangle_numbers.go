e2b33eefae658c4aed73d0ca419363e7eec33cea
Adam Cardenas <cevaris@gmail.com>
2014-12-04 15:37:50 -0700

    got submission, refactoring

--- go/src/triangle_numbers/triangle_numbers.go
+++ go/src/triangle_numbers/triangle_numbers.go
@@ -3,49 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
-	"errors"
 )
-
-func genPascalRow(n int) ([]int, error) {
-	pascalRow := make([]int, n+1)
-	//pascalRow := []int{1}
-	pascalRow[0] = 1
-	
-	for k := 0; k < n; k++ {
-		//fmt.Print(n,",", k, ",", pascalRow[k] * (n - k) / (k+1), " ")
-		pascalRow[k+1] = pascalRow[k] * (n - k) / (k+1)
-		//pascalRow = append(pascalRow, pascalRow[k] * (n - k) / (k+1))
-	}
-	//fmt.Println()
-	return pascalRow, nil
-}
-
-func firstEven(arr []int) (int, error) {
-	for i, val := range arr {
-		if val % 2 == 0 {
-			return i, nil
-		}
-	}
-	return -1, errors.New("Even value not found")
-}
 
 
 func main() {
 	testCases, _ := parseHackerRank()
 
 	for _, t  := range testCases {
-		if pascalRow, err := genPascalRow(t); err != nil {
-			log.Println(err)
-		} else {
-			if ret, err := firstEven(pascalRow); err != nil {
-				fmt.Println(t, pascalRow,ret)
+		if t % 2 == 0 {
+			if (t / 2) % 2 == 0 {
+				fmt.Println(3)
 			} else {
-				// Need to handle the offbyone answer
-				fmt.Println(t, pascalRow, ret+1)
+				fmt.Println(4)
 			}
+		} else {
+			fmt.Println(2)
 		}
 	}
-	
 }
 
 
