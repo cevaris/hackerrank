5733ba3e4143d1f39f14598ec59661092aaa10fb
Adam Cardenas <cevaris@gmail.com>
2014-12-12 15:30:45 -0700

    updated go build script

--- go/build.sh
+++ go/build.sh
@@ -4,7 +4,8 @@
 export GOPATH=$(pwd)
 export GOBIN=$GOPATH/bin
 
-DIRS=$(ls src/**/*.go)
+# DIRS=$(ls src/**/*.go)
+DIRS=$(find $(PWD) -type f -name "*.go")
 
 for f in $DIRS; do
     # echo building $f...
